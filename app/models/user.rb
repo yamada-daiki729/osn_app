a0b88658ea3829b237dd66d0fcf8d24957b0fbe1
daiki_yamada <daiki.yamada@startup-technology.com>
2025-01-12 19:36:21 +0900

    add: devise user ログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :authentication_keys => [:login_id]
+
+
+  # ユーザー名で検索
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login_id = conditions.delete(:login_id)
+      where(conditions).where(login_id: login_id).first
+    else
+      where(conditions).first
+    end
+  end
+
+  # 登録時に email を不要にする
+  def email_required?
+    false
+  end
+  def email_changed?
+    false
+  end
 end
