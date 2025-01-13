6ecf9687be0967e41ab5319994d8ae1e54e43ef2
daiki_yamada <daiki.yamada@startup-technology.com>
2025-01-14 00:12:05 +0900

    add: first_set_password

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,6 +11,7 @@ class Admin::UsersController < Admin::ApplicationController
 
   def create
     user = User.new(user_params)
+    first_set_random_values(user)
     if user.save
       redirect_to admin_users_path, notice: "ユーザーを作成しました"
     else
@@ -22,6 +23,15 @@ class Admin::UsersController < Admin::ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:login_id, :password, :password_confirmation)
+    params.require(:user).permit(:name)
+  end
+
+  def first_set_random_values(user)
+    random_login_id = SecureRandom.alphanumeric(8)
+    random_first_password = SecureRandom.alphanumeric(8)
+    user.login_id = random_login_id
+    user.password = random_first_password
+    user.password_confirmation = random_first_password
+    user.first_login_password = random_first_password
   end
 end
