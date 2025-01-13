abf265913c0b66ecb609b5aaddeeb70cdc2355de
daiki_yamada <daiki.yamada@startup-technology.com>
2025-01-13 23:39:43 +0900

    remove: devise rememberable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :authentication_keys => [:login_id]
+         :recoverable, :validatable, :authentication_keys => [:login_id]
 
   enum :admin_type, { general: 0, admin: 1, super_admin: 2 }
 
